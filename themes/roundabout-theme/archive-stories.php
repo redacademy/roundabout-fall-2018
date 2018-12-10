2dfd8aa14b584c45befeea04d4db7f465a919fe0
Tony <tony_possamai@hotmail.com>
2018-12-10 12:17:41 +0000

    Styling Stories and Single Stories for tablet and desktop

--- themes/roundabout-theme/archive-stories.php
+++ themes/roundabout-theme/archive-stories.php
@@ -38,11 +38,11 @@ $stories = new WP_Query($args);
 	
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					get_template_part( 'template-parts/content', 'stories' );
-				?>
-
+				<div class="stories-wrapper">
+					<?php
+						get_template_part( 'template-parts/content', 'stories' );
+					?>
+				</div>
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
