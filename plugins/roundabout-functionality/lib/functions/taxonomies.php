1c439e187c1e90dac1ecd1a5309fb247edf3a723
aswistowska <agata@swistowski.org>
2018-12-13 12:08:39 +0000

    Revert "News"

--- plugins/roundabout-functionality/lib/functions/taxonomies.php
+++ plugins/roundabout-functionality/lib/functions/taxonomies.php
@@ -49,6 +49,7 @@ function custom_taxonomy() {
 add_action( 'init', 'custom_taxonomy', 0 );
 
 
+
 /**
   * Create "Supporters Members" taxonomies
 **/
@@ -76,7 +77,6 @@ function custom_supporters_taxonomy() {
 		'items_list'                 => __( 'Items list', 'text_domain' ),
 		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
 	);
-
 	$argsSupporters = array(
 		'labels'                     => $labelsSupporters,
 		'hierarchical'               => true,
@@ -86,92 +86,7 @@ function custom_supporters_taxonomy() {
 		'show_in_nav_menus'          => true,
 		'show_tagcloud'              => true,
 	);
-
 	register_taxonomy( 'supporters-type', array( 'supporter' ), $argsSupporters );
 //same name to register_taxonomy
 }
 add_action( 'init', 'custom_supporters_taxonomy', 0 );
-
-  
-// Register Custom Taxonomy for Single NEWS Page Tags
-
-function news_taxonomy() {
-
-	$labels = array(
-		'name'                       => _x( 'News Tags', 'Taxonomy General Name', 'text_domain' ),
-		'singular_name'              => _x( 'News Tag', 'Taxonomy Singular Name', 'text_domain' ),
-		'menu_name'                  => __( 'News Tags', 'text_domain' ),
-    'all_items'                  => __( 'All Items', 'text_domain' ),
-		'parent_item'                => __( 'Parent Item', 'text_domain' ),
-		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
-		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
-		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
-		'update_item'                => __( 'Update Item', 'text_domain' ),
-		'view_item'                  => __( 'View Item', 'text_domain' ),
-		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
-		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-		'popular_items'              => __( 'Popular Items', 'text_domain' ),
-		'search_items'               => __( 'Search Items', 'text_domain' ),
-		'not_found'                  => __( 'Not Found', 'text_domain' ),
-		'no_terms'                   => __( 'No items', 'text_domain' ),
-		'items_list'                 => __( 'Items list', 'text_domain' ),
-		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
-
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-    'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-
-	register_taxonomy( 'news-tags', array( 'news' ), $args );
-
-}
-add_action( 'init', 'news_taxonomy', 0 );
-
-
-
-// Register Custom Taxonomy to create Featured Story
-
-function featured_taxonomy() {
-
-	$labels = array(
-		'name'                       => _x( 'Featured Story', 'Taxonomy General Name', 'text_domain' ),
-		'singular_name'              => _x( 'Featured Story', 'Taxonomy Singular Name', 'text_domain' ),
-		'menu_name'                  => __( 'Featured', 'text_domain' ),
-		'all_items'                  => __( 'All Items', 'text_domain' ),
-		'parent_item'                => __( 'Parent Item', 'text_domain' ),
-		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
-		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
-		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
-		'update_item'                => __( 'Update Item', 'text_domain' ),
-		'view_item'                  => __( 'View Item', 'text_domain' ),
-		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
-		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-		'popular_items'              => __( 'Popular Items', 'text_domain' ),
-		'search_items'               => __( 'Search Items', 'text_domain' ),
-		'not_found'                  => __( 'Not Found', 'text_domain' ),
-		'no_terms'                   => __( 'No items', 'text_domain' ),
-		'items_list'                 => __( 'Items list', 'text_domain' ),
-		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
-	);
-  
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-	);
-	register_taxonomy( 'featured-story', array( 'stories' ), $args );
-
-}
-add_action( 'init', 'featured_taxonomy', 0 );
