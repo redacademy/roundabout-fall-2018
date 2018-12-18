d707fba2c8b1503f24d42f67d8b50d7775f9791a
Tony <tony_possamai@hotmail.com>
2018-12-18 18:41:26 +0000

    Working on API function for the donate page

--- themes/roundabout-theme/js/donation-id.js
+++ themes/roundabout-theme/js/donation-id.js
@@ -2,10 +2,20 @@
 * donation-id.js
 */
 
- // Function to get the donation's ID to redirect back to the website in a "thank you" page
+ // Function to get the details of a donation by donation ID.
+
  
  $( document ).ready(function() {
 
+//  window.location.search.split('=')[1]
 
- 
+
+  const Http = new XMLHttpRequest();
+  const url='https://link.justgiving.com/v1/charity/donate/charityId/248692?amount=10.00&currency=GBP&reference=website&exitUrl=http%3A%2F%2Fwww.roundaboutdramatherapy.org.uk%3FjgDonationId%3DJUSTGIVING-DONATION-ID&message=Thanks%20for%20your%20donation!';
+  Http.open("GET", url);
+  Http.send();
+  Http.onreadystatechange=(e)=>{
+  console.log(Http.responseText)
+  }
+
   }); 
